687b91369457bb2b8e254435f6300b2a6b7545d9
Aliqaan <alikaan.antalya@gmail.com>
2025-01-18 14:27:55 +0300

    example test controller implemented

---
+++ src/main/kotlin/org/example/cinematch/controllers/TestController.kt
@@ -0,0 +1,10 @@
+package org.example.cinematch.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/test")
+    fun test(): String = "test"
+}
