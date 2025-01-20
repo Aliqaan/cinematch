a760b3689c420486fcef44196e33898fb5a524df
Aliqaan <alikaan.antalya@gmail.com>
2025-01-20 22:54:47 +0300

    ci migrate step removed and flyway disabled for tests

--- src/test/kotlin/org/example/cinematch/CineMatchApplicationTests.kt
+++ src/test/kotlin/org/example/cinematch/CineMatchApplicationTests.kt
@@ -2,8 +2,10 @@ package org.example.cinematch
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(properties = ["spring.flyway.enabled=false"])
 class CineMatchApplicationTests {
     @Test
     fun contextLoads() {
