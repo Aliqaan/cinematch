4d0b5409d2937c7e6ce677d185768bbfaa238a89
Aliqaan <alikaan.antalya@gmail.com>
2025-01-20 23:10:49 +0300

    test properties file added and unnecessary flyway line of tests removed

--- src/test/kotlin/org/example/cinematch/CineMatchApplicationTests.kt
+++ src/test/kotlin/org/example/cinematch/CineMatchApplicationTests.kt
@@ -2,10 +2,8 @@ package org.example.cinematch
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
-@TestPropertySource(properties = ["spring.flyway.enabled=false"])
 class CineMatchApplicationTests {
     @Test
     fun contextLoads() {
